4e4e6d6d6d1cba53c4eafd06424354d8ef54d7c5
xiaoma <1286509063@qq.com>
2019-05-27 14:58:38 +0800

    重构，把验错的逻辑写到TEST_ERROR里面

--- test.c
+++ test.c
@@ -21,44 +21,33 @@ static int test_pass = 0;
 
 #define EXPECT_EQ_DOUBLE(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%.17g")
 
+#define TEST_ERROR(error, json)\
+	do{\
+		lept_value v;\
+		v.type = LEPT_FALSE;\
+		EXPECT_EQ_INT(error, lept_parse(&v, json));\
+		EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));\
+	}while(0)
+
 static void test_parse_null() {
 	lept_value v;
 	v.type = LEPT_FALSE;  //这里仅是赋一个初始值而已，没有其他的意义。经过lept_parse函数，该变量都会变为初始值为LEPT_NULL。
 	EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "null"));
 	EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
-
-	v.type = LEPT_FALSE;  
-	EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE, lept_parse(&v, "nullt"));
-	EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
 
 static void test_parse_expect_value() {//检测字符串是否只有空白
-	lept_value v;
-	v.type = LEPT_FALSE;
-	EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE, lept_parse(&v, ""));
-	EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v)); //没有解析成功的话，v默认是赋值为LEPT_NULL
-
-	v.type = LEPT_FALSE;
-	EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE, lept_parse(&v, " "));
-	EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v)); //没有解析成功的话，v默认是赋值为LEPT_NULL
+	TEST_ERROR(LEPT_PARSE_EXPECT_VALUE, "");
+	TEST_ERROR(LEPT_PARSE_EXPECT_VALUE, " ");
 }
 
 static void test_parse_invalid_value() {
-	lept_value v;
-	v.type = LEPT_FALSE;
-	EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE, lept_parse(&v, "nul"));
-	EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
-
-	v.type = LEPT_FALSE;
-	EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE, lept_parse(&v, "?"));
-	EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+	TEST_ERROR(LEPT_PARSE_INVALID_VALUE, "nul");
+	TEST_ERROR(LEPT_PARSE_INVALID_VALUE, "?");
 }
 
 static void test_root_not_singular() {
-	lept_value v;
-	v.type = LEPT_FALSE;
-	EXPECT_EQ_INT(LEPT_PARSE_ROOT_NOT_SINGULAR, lept_parse(&v, "n a"));
-	EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+	TEST_ERROR(LEPT_PARSE_ROOT_NOT_SINGULAR, "n a");
 }
 
 static void test_parse_true() {
@@ -116,7 +105,7 @@ static void test_parse() {
 	//test_root_not_singular();
 	//test_parse_true();
 	//test_parse_false();
-	test_parse_number();
+	//test_parse_number();
 }
 
 int main() {
